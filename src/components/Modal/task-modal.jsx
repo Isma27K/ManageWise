2d8dbcad7ca80d8fae63c619538d0dda7e09c585
ismasalalu <binhelati@gmail.com>
2024-10-06 09:22:52 +0800

    smooth the transition beetween update view and update modal

--- src/components/Modal/task-modal.jsx
+++ src/components/Modal/task-modal.jsx
@@ -1,8 +1,10 @@
 import React, { useState } from 'react';
 import { Modal } from 'antd';
+import { CSSTransition } from 'react-transition-group';
 import CreateTaskModal from './mode/create-task-modal';
 import UpdateTaskModal from './mode/update-task-modal';
 import UpdateTask from './mode/update-task/update-task';
+import './task-modal.css'; // We'll create this CSS file next
 
 const TaskModal = ({ visible, onCancel, pool, task, isEditable, maxTaskNameLength = 40 }) => {
     const [showUpdateTask, setShowUpdateTask] = useState(false);
@@ -18,44 +20,56 @@ const TaskModal = ({ visible, onCancel, pool, task, isEditable, maxTaskNameLengt
         setUpdatedTaskData(null);
     };
 
-    if (showUpdateTask) {
-        return (
-            <UpdateTask
-                visible={showUpdateTask}
-                onCancel={handleUpdateTaskCancel}
-                task={updatedTaskData}
-                isEditable={isEditable}
-                maxTaskNameLength={maxTaskNameLength}
-            />
-        );
-    }
-
     return (
-        <Modal
-            title={task ? `${task.name}` : 'Create New Task'}
-            visible={visible}
-            onCancel={onCancel}
-            footer={null}
-            width={task ? "80vw" : "40vw"}
-            style={{ top: 20 }}
-            centered
-        >
-            {task ? (
-                <UpdateTaskModal
-                    task={task}
+        <>
+            <CSSTransition
+                in={visible && !showUpdateTask}
+                timeout={300}
+                classNames="fade"
+                unmountOnExit
+            >
+                <Modal
+                    title={task ? `${task.name}` : 'Create New Task'}
+                    visible={visible && !showUpdateTask}
+                    onCancel={onCancel}
+                    footer={null}
+                    width={task ? "80vw" : "40vw"}
+                    style={{ top: 20 }}
+                    centered
+                >
+                    {task ? (
+                        <UpdateTaskModal
+                            task={task}
+                            isEditable={isEditable}
+                            maxTaskNameLength={maxTaskNameLength}
+                            onCancel={onCancel}
+                            onUpdateClick={handleUpdateClick}
+                        />
+                    ) : (
+                        <CreateTaskModal
+                            pool={pool}
+                            maxTaskNameLength={maxTaskNameLength}
+                            onCancel={onCancel}
+                        />
+                    )}
+                </Modal>
+            </CSSTransition>
+
+            <CSSTransition
+                in={showUpdateTask}
+                timeout={300}
+                classNames="fade"
+                unmountOnExit
+            >
+                <UpdateTask
+                    visible={showUpdateTask}
+                    onCancel={handleUpdateTaskCancel}
+                    task={updatedTaskData}
                     isEditable={isEditable}
                     maxTaskNameLength={maxTaskNameLength}
-                    onCancel={onCancel}
-                    onUpdateClick={handleUpdateClick}
                 />
-            ) : (
-                <CreateTaskModal
-                    pool={pool}
-                    maxTaskNameLength={maxTaskNameLength}
-                    onCancel={onCancel}
-                />
-            )}
-        </Modal>
+            </CSSTransition>
+        </>
     );
 };
 
